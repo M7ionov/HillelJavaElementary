b5fd6e78e1fdba125f0e1e7a2db645e4b41349c9
M7ionov <m7ionov@ukr.net>
2020-05-11 18:26:41 +0300

    Lesson-02-05 Ввести число, определить четное или нет.

---
+++ Lesson-02-05/src/com/Main.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ввести число, определить четное или нет.");
+
+        System.out.println("Please enter the number");
+        int x = scanner.nextInt();
+        if (x%2==0)
+            System.out.print("You entered the number" + x + "it is even");
+            else
+                System.out.print("You entered the number\" + x + \"it is odd");
+    }
+}
