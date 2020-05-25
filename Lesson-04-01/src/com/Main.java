3d8b673085dd8dcb8129f158ef32f04169f20330
M7ionov <m7ionov@ukr.net>
2020-05-26 01:17:50 +0300

    Lesson-04-01 - Систему учета студентов переделать в ООП стиле

--- Lesson-04-01/src/com/Main.java
+++ Lesson-04-01/src/com/Main.java
@@ -3,17 +3,106 @@ package com;
 import java.util.Arrays;
 
 public class Main {
-
-    //High cohesion - Высокая сплоченность
+    final static int MAX_STUDENTS_IN_GROUP = 20;
+    final static int LESSONS_NUM = 12;
 
     public static void main(String[] args) {
-        Student ivanov = new Student("Ivanov");
-        Student petrov = new Student("Petrov", 10);
-        //ivanov.surname = "Ivanov";
-        //ivanov.marks = new int[32];
-        System.out.println(ivanov.surname);
-        System.out.println(Arrays.toString(ivanov.marks));
-        System.out.println(petrov.surname);
-        System.out.println(Arrays.toString(petrov.marks));
+        Student[] students = new Student[0];
+        print(students);
+        students = addStudent("Ivanov", students);
+        students = addStudent("Petrov", students);
+        students = addStudent("Ivanov", students);
+        students = addStudent("Sidorov", students);
+        addMarks(students);
+        print(students);
+        Student student = findStudent("Sidorov", students);
+        setMark(student, 5, 5);
+        contains(student);
+        print(students);
+        delete("Ivanov", students);
+        students = addStudent("Abramov", students);
+        addMarks(students);
+        print(students);
+
+
+
+    }
+    public static Student[] addStudent(String newStudentName, Student[] oldStudents) {
+        Student[] result = new Student[oldStudents.length +1];
+        for (int i=0; i<oldStudents.length; i++) {
+            result[i] = oldStudents[i];
+        }
+        result[oldStudents.length]= new Student(newStudentName);
+        return result;
+    }
+
+    public static void print(Student[] students){
+        System.out.println();
+        for (int i=0; i<students.length; i++){
+            if (students[i] == null){
+                continue;
+            }
+            System.out.println("Surname - " + students[i].surname+"\nPresence = "+(Arrays.toString(students[i].presence)+"\nMarks = \t"+Arrays.toString(students[i].marks )));
+        }
+    }
+
+    public static void contains(Student student){
+        System.out.println();
+        System.out.println(student.surname);
+        System.out.println("His marks = "+Arrays.toString(student.marks));
+        System.out.println("He present = "+Arrays.toString(student.presence));
+    }
+
+    public static void addMarks(Student[] students) {
+        for (int i = 0; i < students.length ; i++) {
+            if (students[i] != null)  {
+                for (int j = 0; j < students[i].marks.length; j++) {
+                    if (students[i].marks[j] == 0) {
+                        students[i].marks[j] = (int) (Math.random() * 5);
+                        if (students[i].marks[j] > 0)
+                            students[i].presence[j] = true;
+                        else
+                            students[i].presence[j] = false;
+                    }
+                }
+            }
+        }
+    }
+
+    public static Student[] sortAbc(String newStudentName, Student[] oldStudents) {
+        Student[] result = new Student[oldStudents.length +1];
+        for (int i=0; i<oldStudents.length; i++) {
+            result[i] = oldStudents[i];
+        }
+        result[oldStudents.length]= new Student(newStudentName);
+        return result;
+    }
+     /*   public static void clearAll(String[] surnames, int[][] marks, boolean[][] presence){
+            for (int i=0; i<surnames.length; i++){
+                surnames[i]=null;
+                marks[i]=null;
+               presence[i]=null;
+            }
+        }*/
+
+    public static void delete(String toDelete, Student[] students){
+        for (int i=0; i<students.length; i++){
+            if (students[i]!=null && students[i].surname.equalsIgnoreCase(toDelete)){
+                students[i]=null;
+             }
+        }
+    }
+
+    public static void setMark(Student student, int mark, int lessonNum){
+            student.marks[lessonNum-1]=mark;
+    }
+
+    public static Student findStudent(String surname, Student[] students){
+        for (int i=0; i<students.length; i++){
+            if (students[i]!=null && students[i].surname.equalsIgnoreCase(surname)){
+                return students[i];
+            }
+        }
+        return null;
     }
 }
--- Lesson-04-01/src/com/Student.java
+++ Lesson-04-01/src/com/Student.java
@@ -1,20 +1,17 @@
 package com;
 
 public class Student {
-    //state - состояние
     String surname;
     int[] marks;
     boolean[] presence;
 
-    public Student(String s){
-        this(s, 16);
+    public Student(String surname){
+        this.surname = surname;
+        this.marks = new int[Main.LESSONS_NUM];
+        this.presence = new boolean[Main.LESSONS_NUM];
     }
 
-    public Student(String s, int numOfLessons){
-        surname = s;
-        marks = new int[numOfLessons];
-        presence = new boolean[numOfLessons];
-    }
 
-    //behavior - поведение
+
+
 }
