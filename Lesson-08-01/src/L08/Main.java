d62cf93064e6c586e7e96184c184e8bd11daac90
M7ionov <m7ionov@ukr.net>
2020-08-05 17:37:22 +0300

    Lesson-08-01 Создать свою коллекцию (имплементировать интерфейс Collection).

--- Lesson-08-01/src/L08/Main.java
+++ Lesson-08-01/src/L08/Main.java
@@ -1,4 +1,5 @@
 package L08;
+
 import java.util.Scanner;
 
 //Создать свою коллекцию (имплементировать интерфейс Collection)
@@ -8,11 +9,16 @@ public class Main {
     public static void main(String[] args) {
         Container students = new Container();
 
-        students.add(new Student("ivanov"));
-        addStudentFromUserInput(students);
+        System.out.println(students.add(new Student("ivanov")));
+//        addStudentFromUserInput(students);
+        students.add(new Student("petrov"));
         students.add(new Student("sidorov"));
         System.out.println(students);
-        System.out.println(students.contains(new Student("sidorov")));
+        System.out.println(students.contains(new Student("sidorov")) + " - contains sidorov");
+        System.out.println(students.size());
+//        System.out.println(students.remove(new Student("petrov")) + " - remove petrov");
+        System.out.println(students.size());
+        System.out.println(students);
 
         Container students2 = new Container();
         students2.add(new Student("ivanidze"));
@@ -22,39 +28,34 @@ public class Main {
 
         students.addAll(students2);
         System.out.println(students);
-
         System.out.println("add all = " + students.size());
-        System.out.println(students.remove(new Student("petridze")));
+
+        System.out.println(students.contains(new Student("petridze")) + " - contains petridze");
+//        System.out.println(students.remove(new Student("petridze")) + " - remove petridze");
         System.out.println(students);
-        System.out.println("remove = " + students.size());
+        System.out.println(students.size());
 
-        Container students3 = new Container();
-        students3.add(new Student("sidorov"));
-        students3.add(new Student("petrov"));
-        students3.add(new Student("ivanidze"));
-        students3.add(new Student("sidoridze"));
+//        Container students3 = new Container();
+//        students3.add(new Student("sidorov"));
+//        students3.add(new Student("petrov"));
+//        students3.add(new Student("ivanidze"));
+//        students3.add(new Student("sidoridze"));
 
-//        students.retainAll(students3);
-        System.out.println(students);
-        System.out.println("retainAll = " + students.size());
-
-//        students.removeAll(students2);
+//        System.out.println(students.retainAll(students2));
 //        System.out.println(students);
-//        System.out.println(students.size());
+//        System.out.println("retainAll = " + students.size());
 
-        System.out.println("out s - " + students.remove(new Student("sidorov")));
-        System.out.println("remove si = " + students.size());
-
-        System.out.println("out i - " + students.remove(new Student("ivanov")));
-        System.out.println("remove iv = " + students.size());
-
+//        System.out.println(students.removeAll(students2));
+//        System.out.println(students);
+//        System.out.println("removeAll = " + students.size());
+//
         System.out.println(students);
-//        System.out.println(students.containsAll(students2));
+        System.out.println(students.containsAll(students2));
         System.out.println("contains all = " + students.size());
-
-        students.clear();
-        System.out.println(students);
-        System.out.println("is empty = " + students.isEmpty());
+//
+//        students.clear();
+//        System.out.println(students);
+//        System.out.println("is empty = " + students.isEmpty());
     }
 
     public static void addStudentFromUserInput(Container students) {
